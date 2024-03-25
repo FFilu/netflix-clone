3f9b346a336582ea13b39757c6178307d62cbb68
FLAVIU\flaviu <flaviu.daniel26@gmail.com>
2024-03-25 21:42:19 +0200

    auth page

--- componets/Input.tsx
+++ componets/Input.tsx
@@ -13,45 +13,20 @@ const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
     <div className="relative">
       <label 
         htmlFor={id} 
-        className="absolute text-mb duration-150
-        transform 
-        -translate-y-3 
-        scale-75 
-        top-4 
-        z-10 
-        origin-[0] 
-        left-6
-        text-white
-        peer-placeholder-shown:scale-100 
-        peer-placeholder-shown:translate-y-0 
-        peer-focus:scale-75
-        peer-focus:-translate-y-3
-      ">{label}</label>
+        className="absolute text-white text-md duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
+      >
+        {label}
+      </label>
       <input
         onChange={onChange}
         value={value}
         type={type}
         id={id}
-        className="
-        block
-        rounded-md
-        px-6
-        pt-6
-        pb-1
-        w-full
-        text-md
-      text-black
-      bg-neutral-700
-        appearance-none
-        focus:outline-none
-        focus:ring-0
-        peer
-        invalid:border-b-1
-        "
-        placeholder=" " 
+        className="block rounded-md px-6 pt-6 pb-1 w-full text-md text-black bg-neutral-700 appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1"
+        placeholder=" "
       />
-      </div>
-  )
+    </div>
+  );
 }
 
 export default Input;
